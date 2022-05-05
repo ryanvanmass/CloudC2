138c197cd96fde784a29181cb21c96398a6cf9a2
Ryan <ryanvanmassenhoven@gmail.com>
2022-05-05 10:20:37 -0400

    Add ability to choose between HTTP and HTTPS

--- Start.sh
+++ Start.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-./c2-*_amd64_linux -https -hostname $1
+# $1 = the hostname (URL) or IP Adress
+
+# HTTPS
+#./C2/c2-*_amd64_linux -https -hostname $1
+
+# HTTP
+./C2/c2-*_amd64_linux -hostname $1
