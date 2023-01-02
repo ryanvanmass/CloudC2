a531e270a4cf5148d801967d103c9d351be798f6
Ryan Van Massenhoven <ryanvanmassenhoven@gmail.com>
2023-01-02 11:16:31 -0500

    Fix HTTPS command to include db file

--- Start.sh
+++ Start.sh
@@ -2,7 +2,7 @@
 # $1 = the hostname (URL) or IP Adress
 
 # HTTPS
-#./C2/c2-*_amd64_linux -https -hostname $1
+#./C2/c2-*_amd64_linux -https -hostname $1 -db /C2/c2.db >> /C2/C2.log
 
 # HTTP
 ./C2/c2-*_amd64_linux -hostname $1 -db /C2/c2.db >> /C2/C2.log
